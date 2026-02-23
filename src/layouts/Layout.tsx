d34946f7484be04aecdc5129190ab19054252139
haini000 <haini000@naver.com>
2026-02-24 01:01:33 +0900

    fix: vercel 오류 해결

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -3,6 +3,10 @@ import Header from "../components/user/header/Header";
 import { Link, Outlet } from "react-router-dom";
 
 const Layout = () => {
+
+  //vercel 배포 오류 link 사용 시 제거 
+  console.log(Link);
+  
   return (
     <div>
       <Header/>
